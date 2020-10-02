3ba1f0b9161852f8b0cc569bd11df1a7a35e6d54
Lucas Impieri <lucasimpieri@gmail.com>
2020-10-02 15:07:34 -0300

    Exception handling - delete

--- src/main/java/com/works/projeto/services/UserService.java
+++ src/main/java/com/works/projeto/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.works.projeto.entities.User;
 import com.works.projeto.repositories.UserRepository;
+import com.works.projeto.services.exceptions.DatabaseException;
 import com.works.projeto.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
---
+++ src/main/java/com/works/projeto/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.works.projeto.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+}
